87efea33faf4fc919bf0e5faa4c7df5032f86881
asher8346 <asher.schulman@gmail.com>
2021-04-04 10:43:30 -0500

    begin search bar js functionality

--- js/app.js
+++ js/app.js
@@ -5,65 +5,85 @@ if (typeof $ == 'undefined') {
 };
 //variables for ajax requests
 const baseURL = 'https://rickandmortyapi.com/api/character';
-let currentPage = 1;
 const page = '?page=';
+let currentPage = 1;
 let filter = '';
+let queryURL = baseURL + page + currentPage + filter;
 //$.ajax() request for character data
-const getRequest = () => {
+const getRequest = (url) => {
     $.ajax({
-            url: baseURL + page + currentPage + filter,
+            url: queryURL,
             type: 'GET',
         })
         .then(results = (data) => {
+            console.log(queryURL)
             const allData = data
             console.log(allData)
             const array = data.results
             console.log(array)
+            const $cardContainer = $('#cardContainer');
+            $cardContainer.empty();
             for (let i = 0; i < array.length; i++) {
-                console.log(`adding ${array[i].name} to a card`)
-                const $cardContainer = $('#cardContainer');
-                const $card = $('<div>').addClass('card').attr('id', `card${array[i].id}`)
-                const $cardImg = $('<img>').addClass('cardImg').attr('src', `${array[i].image}`)
+                console.log(`adding ${array[i].name} to a card`);
+                // const $cardContainer = $('#cardContainer');
+                const $card = $('<div>').addClass('card').attr('id', `card${array[i].id}`);
+                const $cardImg = $('<img>').addClass('cardImg').attr('src', `${array[i].image}`);
                 const $cardTextContainer = $('<div>').addClass('cardTextContainer').html(`
-            <h3 class="cardText">${array[i].name}</h3>
-            <p><span class='title'>Species: </span>${array[i].species}</p>
-            <p><span class='title'>Subspecies (if applicable): </span>${array[i].type}</p>
-            <button class='moreButton'>More information (not functional yet)</button>
-            `)
-                $card.append($cardImg)
-                $card.append($cardTextContainer)
-                $cardContainer.append($card)
+                    <h3 class="cardText">${array[i].name}</h3>
+                    <p><span class='title'>Species: </span>${array[i].species}</p>
+                    <p><span class='title'>Subspecies (if applicable): </span>${array[i].type}</p>
+                    <button class='moreButton'>More information (not functional yet)</button>
+                    `);
+                $card.append($cardImg);
+                $card.append($cardTextContainer);
+                $cardContainer.append($card);
             }
         })
 }
 
 const listeners = {
     id: 'object storing functions',
-    next: function nextPage(event) {
+    //on next button click
+    next: function (event) {
         event.preventDefault();
         $('#cardContainer').empty();
         console.log('changing pages')
         currentPage += 1
         if (currentPage > 34) {
             alert('you are already on the last page')
-            currentPage = 1;
         }
+        queryURL = baseURL + page + currentPage + filter;
         let $currentPage = $('.pg')
         $currentPage.text(currentPage);
         getRequest()
     },
-    prev: function prevPage(event) {
+    //on prev button click
+    prev: function (event) {
         event.preventDefault();
         $('#cardContainer').empty();
         console.log('changing pages')
         currentPage -= 1
         if (currentPage < 1) {
             alert('you are already on the first page')
-            currentPage = 34;
         }
+        queryURL = baseURL + page + currentPage + filter;
         let $currentPage = $('.pg')
         $currentPage.text(currentPage);
-        getRequest()
+        getRequest();
+    },
+    //on search button click
+    ///!!! SOMETHING is wrong here, loads first page of rick but nothing else and getting errors when trying to do other stuff
+    search: function (event) {
+        event.preventDefault();
+        const $searchinput = $('#searchbar').val();
+        console.log('clicking now');
+        console.log($searchinput);
+        //more code here
+        filter = `&name=${$searchinput}`;
+        currentPage = 1
+        // queryURL = baseURL + page + 1 + filter;
+        queryURL = baseURL + page + currentPage + filter;
+        getRequest();
     }
 };
 // ended up putting both of these functions, withing my 'listeners' object and calling upon those methods during my event listener click events
@@ -107,5 +127,5 @@ $(() => {
     $('.previous').on('click', listeners.prev);
     // $('.previous').on('click', prevPage);
 
-    $('#search').on('click', searchFilter);
+    $('#search').on('click', listeners.search);
 });
